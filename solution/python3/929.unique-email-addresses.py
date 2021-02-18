829497f7be37d4f8446fc296104b7a9c518d0189
uu64 <yusuke.t.0808@gmail.com>
2021-02-18 23:49:22 +0900

    add: 349, 929

---
+++ solution/python3/929.unique-email-addresses.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=929 lang=python3
+#
+# [929] Unique Email Addresses
+#
+
+# @lc code=start
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        note = collections.defaultdict(set)
+        for mail in emails:
+            tmp = mail.split("@")
+            note[tmp[1]].add(tmp[0].split("+")[0].replace(".", ""))
+
+        ans = 0
+        for v in note.values():
+            ans += len(v)
+        return ans
+# @lc code=end
+
