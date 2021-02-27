9bddba1a7b9d8141250b17d1c99ac3b153216cb1
uu64 <yusuke.t.0808@gmail.com>
2021-02-27 16:39:40 +0900

    fix: 98

--- solution/python3/98.validate-binary-search-tree.py
+++ solution/python3/98.validate-binary-search-tree.py
@@ -13,25 +13,19 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
-        result = self.search(root)
-        return result[0]
+        leaves = []
+        result = self.pickLeaves(root, leaves)
+        for i in range(1, len(leaves)):
+            if leaves[i-1] >= leaves[i]:
+                return False
+        return True
 
-    def search(self, root):
-        minVal = root.val
-        maxVal = root.val
-        isValid = True
-        if root.left:
-            l = self.search(root.left)
-            isValid = isValid and l[0] and l[2] < root.val
-            minVal = min(minVal, l[1])
-            maxVal = max(maxVal, l[2])
+    def pickLeaves(self, root, leaves):
+        if root:
+            self.pickLeaves(root.left, leaves)
+            leaves.append(root.val)
+            self.pickLeaves(root.right, leaves)
 
-        if root.right:
-            r = self.search(root.right)
-            isValid = isValid and r[0] and root.val < r[1]
-            minVal = min(minVal, r[1])
-            maxVal = max(maxVal, r[2])
 
-        return (isValid, minVal, maxVal)
 # @lc code=end
 
